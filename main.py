98a977654fb0baa9563b8a71d9cfdf222601c2ca
Luan <luan.marko2@gmail.com>
2023-08-10 20:43:59 -0300

    Ainda com bugs

--- main.py
+++ main.py
@@ -5,19 +5,45 @@ import glob
 
 # Caminho para o diretório principal
 caminhoDiretorio= '/home/luan/Desktop/PKLot/PKLot'
+caminhoNovo = '/home/luan/Desktop/PKLot/PKLotSegmentedNovo'
+
+# Criando diretório
+if not os.path.exists(caminhoNovo):
+    os.makedirs(caminhoNovo)
 
 for universidades in os.listdir(caminhoDiretorio):
+    # Caminho para as universidades 
     caminhoUniversidades = os.path.join(caminhoDiretorio, universidades)
+    caminhoUniversidadesNovo = os.path.join(caminhoNovo, universidades)
+
+    #criando diretório
+    os.makedirs(caminhoUniversidadesNovo, exist_ok=True)
 
     for climas in os.listdir(caminhoUniversidades):
+        # Caminho para os climas
         caminhoClimas = os.path.join(caminhoUniversidades, climas)
+        caminhoClimasNovo = os.path.join(caminhoUniversidadesNovo, climas)
+
+        # Criando diretório
+        os.makedirs(caminhoClimasNovo, exist_ok=True)
 
         for datas in os.listdir(caminhoClimas):
+            # Caminho para as datas
             caminhoDatas = os.path.join(caminhoClimas, datas)
+            caminhoDatasNovo = os.path.join(caminhoClimasNovo, datas)
+
+            # Criando diretório
+            os.makedirs(caminhoDatasNovo, exist_ok=True)
 
             # Criando listas para armazenar os arquivos .xml e .jpg
             listaXML = [arq for arq in os.listdir(caminhoDatas) if arq.endswith('.xml')]
             listaJPG = [arq for arq in os.listdir(caminhoDatas) if arq.endswith('.jpg')]
+
+            # Criando pastas Empty e Occupied
+            caminhoEmpty = os.path.join(caminhoDatasNovo, 'Empty')
+            caminhoOccupied = os.path.join(caminhoDatasNovo, 'Occupied')
+            os.makedirs(caminhoEmpty, exist_ok=True)
+            os.makedirs(caminhoOccupied, exist_ok=True)
 
             # Percorrendo as listas
             for i in range(len(listaXML)):
@@ -33,7 +59,7 @@ for universidades in os.listdir(caminhoDiretorio):
                 # Recortando imagens
                 for vaga in xml.findall('.//space'):
                     # ID da vaga e ocupação
-                    vagaID = vaga.get('id')
+                    vagaID = vaga.get('id').zfill(3)
                     ocupacao = vaga.get('occupied')
 
                     # Coordenadas da vaga e ângulo
@@ -58,7 +84,13 @@ for universidades in os.listdir(caminhoDiretorio):
                     else:
                         imagemFinal = recorte
 
-                    # Mostrando a imagem rotacionada
-                    cv2.imshow('Imagem Rotacionada', imagemFinal)
-                    cv2.waitKey(0)
-                    cv2.destroyAllWindows()
+                    # Criando nome da imagem
+                    nomeImagem = datas + "#" + vagaID + ".jpg"
+
+                    # Colocando imagem em Empty ou Occupied
+                    if ocupacao == '0':
+                        cv2.imwrite(os.path.join(caminhoEmpty, "#" + vagaID + '.jpg'), imagemFinal)
+                    elif ocupacao == '1':
+                        cv2.imwrite(os.path.join(caminhoOccupied, "#" + vagaID + '.jpg'), imagemFinal)
+
+                    
